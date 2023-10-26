630d38dfc0affb7ee0cccde75b3bc50032bf8447
sunyixin1 <144147503+sunyixin1@users.noreply.github.com>
2023-10-26 20:44:12 +0800

    Update Global Solution.py

--- pages/Global Solution.py
+++ pages/Global Solution.py
@@ -1,2 +1,10 @@
 import streamlit as st
-st.title('Nothing now')
+
+# title
+st.title("Global solution")
+
+# show HTMLfile
+html_file_path = "graph/map111.html"
+with open(html_file_path, 'r', encoding='utf-8') as f:
+    html_content = f.read()
+    st.components.v1.html(html_content, width=800, height=600)
